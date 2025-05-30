6d8b95711accf48220053783e0478c06d12f09a8
adamkucharski <adam.kucharski@lshtm.ac.uk>
2025-05-30 19:13:50 +0100

    Update 02-practical-activity-1.R

--- instructors/02-practical-activity-1.R
+++ instructors/02-practical-activity-1.R
@@ -10,55 +10,49 @@ library(tidyverse)
 
 
 # Read reported cases -----------------------------------------------------
-# For COVID
-# NOTE: Replace the link with the actual data source
-# dat <- read_rds("paste/link/url/here/covid") %>%
-#   dplyr::select(date, confirm)
-# Or for Ebola
-# dat <- read_rds("paste/link/url/here/ebola") %>%
-#   dplyr::select(date, confirm = cases)
-
+# for covid
 dat <- read_rds("paste/link/url/here/covid") %>%
   dplyr::select(date, confirm)
 # or
-# dat <- read_rds("paste/link/url/here/ebola") %>%
-#   dplyr::select(date, confirm = cases)
+# for ebola
+dat <- read_rds("paste/link/url/here/ebola") %>%
+  dplyr::select(date, confirm = cases)
 
 
 # Define a generation time from {epiparameter} to {EpiNow2} ---------------
 
-# Access a serial interval
+# access a serial interval
 ebola_serialint <- epiparameter::epiparameter_db(
   #<COMPLETE>
 )
 
-# Extract parameters from {epiparameter} object
+# extract parameters from {epiparameter} object
 ebola_serialint_params <- epiparameter::#<COMPLETE>
 
-# Adapt {epiparameter} to {EpiNow2} distribution interface
+# adapt {epiparameter} to {EpiNow2} distribution inferfase
 ebola_generationtime <- EpiNow2::#<COMPLETE>
 
 
 # Define the delays from infection to case report for {EpiNow2} -----------
 
-# Define delay from symptom onset to case report
+# define delay from symptom onset to case report
 ebola_reportdelay <- EpiNow2::#<COMPLETE>
 
-# Define a delay from infection to symptom onset
+# define a delay from infection to symptom onset
 ebola_incubationtime <- epiparameter::epiparameter_db(
   #<COMPLETE>
 )
 
-# Incubation period: extract distribution parameters
+# incubation period: extract distribution parameters
 ebola_incubationtime_params <- epiparameter::#<COMPLETE>
 
-# Incubation period: discretize and extract maximum value (p = 99%)
+# incubation period: discretize and extract maximum value (p = 99%)
 ebola_incubationtime_max <- ebola_incubationtime %>% #<COMPLETE>
 
-# Incubation period: adapt to {EpiNow2} distribution interface
+# incubation period: adapt to {EpiNow2} distribution interface
 ebola_incubationtime_epinow <- EpiNow2::#<COMPLETE>
 
-# Print required input
+# print required input
 ebola_generationtime
 ebola_reportdelay
 ebola_incubationtime_epinow
