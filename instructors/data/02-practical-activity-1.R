50c2fe9840c871817b23dd13051d48261b1cd0b9
Andree Valle Campos <avallecam@gmail.com>
2025-06-30 16:03:54 +0100

    make activity script input neutral

--- instructors/data/02-practical-activity-1.R
+++ instructors/data/02-practical-activity-1.R
@@ -11,51 +11,55 @@ library(tidyverse)
 
 # Read reported cases -----------------------------------------------------
 # for covid
-dat <- read_rds("paste/link/url/here/covid") %>%
+dat <- read_rds(
+  "paste/link/url/here/covid"#<COMPLETE>
+) %>%
   dplyr::select(date, confirm)
 # or
 # for ebola
-dat <- read_rds("paste/link/url/here/ebola") %>%
+dat <- read_rds(
+  "paste/link/url/here/ebola"#<COMPLETE>
+) %>%
   dplyr::select(date, confirm = cases)
 
 
 # Define a generation time from {epiparameter} to {EpiNow2} ---------------
 
 # access a serial interval
-ebola_serialint <- epiparameter::epiparameter_db(
+dat_serialint <- epiparameter::epiparameter_db(
   #<COMPLETE>
 )
 
 # extract parameters from {epiparameter} object
-ebola_serialint_params <- epiparameter::#<COMPLETE>
+dat_serialint_params <- epiparameter::#<COMPLETE>
 
 # adapt {epiparameter} to {EpiNow2} distribution inferfase
-ebola_generationtime <- EpiNow2::#<COMPLETE>
+dat_generationtime <- EpiNow2::#<COMPLETE>
 
 
 # Define the delays from infection to case report for {EpiNow2} -----------
 
 # define delay from symptom onset to case report
-ebola_reportdelay <- EpiNow2::#<COMPLETE>
+dat_reportdelay <- EpiNow2::#<COMPLETE>
 
 # define a delay from infection to symptom onset
-ebola_incubationtime <- epiparameter::epiparameter_db(
+dat_incubationtime <- epiparameter::epiparameter_db(
   #<COMPLETE>
 )
 
 # incubation period: extract distribution parameters
-ebola_incubationtime_params <- epiparameter::#<COMPLETE>
+dat_incubationtime_params <- epiparameter::#<COMPLETE>
 
 # incubation period: discretize and extract maximum value (p = 99%)
-ebola_incubationtime_max <- ebola_incubationtime %>% #<COMPLETE>
+dat_incubationtime_max <- dat_incubationtime %>% #<COMPLETE>
 
 # incubation period: adapt to {EpiNow2} distribution interface
-ebola_incubationtime_epinow <- EpiNow2::#<COMPLETE>
+dat_incubationtime_epinow <- EpiNow2::#<COMPLETE>
 
 # print required input
-ebola_generationtime
-ebola_reportdelay
-ebola_incubationtime_epinow
+dat_generationtime
+dat_reportdelay
+dat_incubationtime_epinow
 
 
 # Set the number of parallel cores for {EpiNow2} --------------------------
