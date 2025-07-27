519a5b918835a83ea63d4e2fc258f99d9c5e62b0
Andree Valle Campos <avallecam@gmail.com>
2025-07-27 14:07:13 +0100

    add all interventions to code

--- instructors/fig/04-practical-instructor-3.R
+++ instructors/fig/04-practical-instructor-3.R
@@ -4,7 +4,7 @@
 # Activity 3
 
 # step: fill in your room number
-room_number <- 2
+room_number <- 1
 
 # Combine interventions --------------------------------------------------
 
@@ -15,9 +15,10 @@ simulate_twointerventions <- epidemics::model_default(
   recovery_rate = recovery_rate,
   # Intervention
   intervention = list(
-    transmission_rate = intervention_mask_mandate,
+    # transmission_rate = intervention_mask_mandate#,
     contacts = intervention_schoolclosure
   ),
+  vaccination = intervention_vaccinate,
   time_end = 1000,
   increment = 1.0
 )
@@ -29,24 +30,30 @@ epidemics::epidemic_peak(simulate_twointerventions)
 
 infections_baseline <- epidemics::new_infections(
   data = simulate_baseline,
-  by_group = FALSE
+  compartments_from_susceptible = "vaccinated", # if vaccination
+  by_group = FALSE # if TRUE, then age-stratified output
 )
 
 infections_twointerventions <- epidemics::new_infections(
   data = simulate_twointerventions,
-  by_group = FALSE
+  compartments_from_susceptible = "vaccinated", # if vaccination
+  by_group = FALSE # if TRUE, then age-stratified output
 )
 
 # Assign scenario names
 infections_baseline$scenario <- "Baseline"
-infections_twointerventions$scenario <- "Mask mandate + School closure"
+infections_twointerventions$scenario <- "School closure + Vaccination"
+# infections_twointerventions$scenario <- "Mask mandate + School closure"
+# infections_twointerventions$scenario <- "Mask mandate + Vaccination"
 
 # Compare interventions --------------------------------------------------
 
 # Combine the data from all scenarios
 compare_interventions <- dplyr::bind_rows(
   infections_baseline,
-  infections_intervention, # varies depending on last one runned
+  infections_schoolclosure, 
+  # infections_mask_mandate,
+  # infections_vaccinate, 
   infections_twointerventions
 )
 
