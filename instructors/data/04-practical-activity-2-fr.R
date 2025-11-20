9b9b43c04494dde1a96477a28851cba648e4778b
Andree Valle Campos <avallecam@gmail.com>
2025-11-20 12:19:04 +0000

    update french translation

--- instructors/data/04-practical-activity-2-fr.R
+++ instructors/data/04-practical-activity-2-fr.R
@@ -1,45 +1,49 @@
 # nolint start
 
-# Practical 4
-# Activity 2
+# Exercice pratique 4
+# Activité 2
 
-# step: fill in your room number
-room_number <- #<COMPLETE> replace with 1/2/3/4
-
+# étape: remplissez le numéro du groupe
+room_number <- #<À COMPLETER> remplacer avec 1/2/3/4
+  
 # Intervention ---------------------------------------------------------
 
-# note: all input parameters come from
-# the table of parameters of the practical document
-  
+# remarque: Tous les paramètres d'entrée proviennent du tableau des paramètres
+# du document pratique
+
 rownames(socialcontact_matrix)
 
-# step: create the intervention object:
+# étape: créer un object de la classe <intervention>
 # 
-# identify if you need to keep: 
-# epidemics::intervention() or epidemics::vaccination()
+# utiliser la fonction epidemics::intervention() ou epidemics::vaccination()
+# avec les arguments suivants
 #
-# then add:
-# - name of the intervention
-# - type of intervention ("rate" or "contacts"), if needed
-# - time when the intervention begins and ends (as values or matrix*)
-# - reduction or vaccination rate (as values or matrix*)
+# - nom de l'intervention
+# - type de l'intervention ("rate" or "contacts"), si nécessaire
+# - les dates de début et de fin de l'intervention (en valeur entière ou sous
+#   forme de matrice*)
+# - le taux de réduction ou vaccination (en valeur réelle or sous la forme d'une
+#   matrice*)
 # 
-# *if matrix, values follow same order as in the social contact matrix
+# *lorsqu'il s'agit d'une matrice, les valeurs doivent suivent l'ordre défini
+# dans la matrice de contact social
 #  
-test_intervention <- epidemics::intervention(#<COMPLETE>
+test_intervention <- epidemics::intervention(
+  #<À COMPLETER>
 )
 # or
-test_intervention <- epidemics::vaccination(#<COMPLETE>
+test_intervention <- epidemics::vaccination(
+  #<À COMPLETER>
 )
 
 test_intervention
 
-# Run {epidemics} ---------------------------------------------------------
+# Exécuter {epidemics} ---------------------------------------------------------
 
-# step: add the intervention argument
+# étape: ajouter les arguments de la fonction
 # 
-# as a list (for interventions against contacts or transmission rate) 
-# or as an object (for vaccination)
+# sous la forme d'une liste (pour les interventions contre les contacts ou le
+# taux de transmission) ou d'un object (pour la vaccination)
 # 
 simulate_intervention <- epidemics::model_default(
   population = population_object,
@@ -47,16 +51,16 @@ simulate_intervention <- epidemics::model_default(
   infectiousness_rate = infectiousness_rate,
   recovery_rate = recovery_rate,
   # Intervention
-  #<COMPLETE>,
+  #<À COMPLETER>,
   time_end = 1000,
   increment = 1.0
 )
 
 simulate_intervention
 
-# Plot all compartments --------------------------------------------------
+# tracer la courbe de tous les compartiments -----------------------------------
 
-# run: paste plot in report
+# exécuter: coller la figure obtenue dans le rapport
 
 simulate_intervention %>%
   ggplot(aes(
@@ -77,46 +81,48 @@ simulate_intervention %>%
   )
 
 
-# Peak of infectious -----------------------------------------------------
+# pic d'infectiosité -----------------------------------------------------
 
-# run: paste table output in report
+# exécuter: coller le tableau obtenu dans le rapport
 
 epidemics::epidemic_peak(data = simulate_intervention)
 
-# Visualize effect --------------------------------------------------------
-# Plot new infections 
+# visualiser les effets des interventions --------------------------------------
+# tracer la courbe des nouvelles infections 
 
-# step: 
-# - add intervention name
-# - if your intervention is vaccination, then
-# - activate the argument "exclude_compartments"
-# - run and paste plot output in report
+# étape: 
+# - ajouter le nom de l'intervention
+# - s'il s'agit d'une vaccination, alors
+# - activer l'argument "exclude_compartments"
+# - exécuter et coller la courbe obtenue dans le rapport
 
 infections_baseline <- epidemics::new_infections(
   data = simulate_baseline,
-  # exclude_compartments = "vaccinated", # if vaccination
-  by_group = FALSE # if TRUE, then age-stratified output
+  # exclude_compartments = "vaccinated", # lorsqu'il s'agit d'une vaccination
+  by_group = FALSE # si TRUE, alors le résultat sera stratifié par groupe d'âge
 )
 
 infections_intervention <- epidemics::new_infections(
   data = simulate_intervention,
-  # exclude_compartments = "vaccinated", # if vaccination
-  by_group = FALSE # if TRUE, then age-stratified output
+  # exclude_compartments = "vaccinated", # lorsqu'il s'agit d'une vaccination
+  by_group = FALSE # si TRUE, alors le résultat sera stratifié par groupe d'âge
 )
 
-# Assign scenario names
+# désigner les noms des scénarios
 infections_baseline$scenario <- "Baseline"
-infections_intervention$scenario <- "ADD Intervention Name" #<COMPLETE>
+infections_intervention$scenario <- "donner le nom de l'intervention" #<À COMPLETER>
 
-# Combine the data from both scenarios
-infections_baseline_intervention <- bind_rows(infections_baseline, infections_intervention)
+# combiner les données de deux scenarios
+infections_baseline_intervention <- dplyr::bind_rows(
+  infections_baseline, infections_intervention
+)
 
 infections_baseline_intervention %>%
   ggplot(aes(
     x = time,
     y = new_infections,
     colour = scenario,
-    # linetype = demography_group # if by_group = TRUE
+    # linetype = demography_group # si by_group = TRUE
   )) +
   geom_line() +
   geom_vline(
