0ec2104cbe163a9c71abfa59afaf12eb2f342856
Degoot-AM <degoot@aims.ac.za>
2026-02-26 09:41:55 +0000

    completing activity files

--- instructors/data/miscellaneous-practical-finalsize-acativity-2.R
+++ instructors/data/miscellaneous-practical-finalsize-acativity-2.R
@@ -38,6 +38,9 @@ contact_matrix <- #<COMPLETE>
 demography_vector <- #<COMPLETE>
 demography_data <- #<COMPLETE>
 
+
+# Transpose and normalize contact matrix---------------------------------------
+
 # scale the contact matrix so the largest eigenvalue is 1.0
 # this is to ensure that the overall epidemic dynamics correctly reflect
 # the assumed value of R0
@@ -48,10 +51,91 @@ contact_matrix <- #<COMPLETE>
 # at random with individuals in group {i}
 contact_matrix <- #<COMPLETE>
 
+# Create susceptibility, and demography-susceptibility distribution matrices---
 n_demo_grps <- #<COMPLETE>
 
+# Number of susceptible groups
+n_susc_groups <- #<COMPLETE>
+# susceptibility level
+susc_guess <- # <COMPLETE>
 
-# Calculate the final size ---------------------------------------------
+# Declare susceptibility matrix
 
+  susc_uniform <- matrix(
+    data = #<COMPLETE>,
+    nrow = #<COMPLETE>,
+    ncol = #<COMPLETE>
+  )
 
-  # nolint end
+# Declare demography-susceptibility distribution matrix
+p_susc_uniform <- matrix(
+  data =  #<COMPLETE>,
+  nrow =  #<COMPLETE>,
+  ncol =  #<COMPLETE>
+)
+
+# Calculate the final size -----------------------------------------------------
+
+final_size_data <- final_size(
+  r0 = #<COMPLETE>,
+  contact_matrix = #<COMPLETE>,
+  demography_vector = #<COMPLETE>,
+  susceptibility = #<COMPLETE>,
+  p_susceptibility = #<COMPLETE>
+)
+
+# View the output data frame
+final_size_data
+
+# Visualize the proportion infected in each demographic group-------------------
+
+# order demographic groups as factors
+final_size_data$demo_grp <- factor(
+  final_size_data$demo_grp,
+  levels = demography_data$age.group
+)
+# plot data
+ggplot(final_size_data) +
+  geom_col(
+    aes(
+      demo_grp, p_infected
+    ),
+    colour = "black", fill = "grey"
+  ) +
+  scale_y_continuous(
+    labels = scales::percent,
+    limits = c(0, 1)
+  ) +
+  expand_limits(
+    x = c(0.5, nrow(final_size_data) + 0.5)
+  ) +
+  theme_classic() +
+  coord_cartesian(
+    expand = FALSE
+  ) +
+  labs(
+    x = "Age group",
+    y = "% Infected"
+  )
+# Visualize the total number infected in each demographic group----------------
+# prepare demography data
+demography_data <- contact_data$demography
+
+# merge final size counts with demography vector
+final_size_data <- merge(
+  final_size_data,
+  demography_data,
+  by.x = "demo_grp",
+  by.y = "age.group"
+)
+
+# reset age group order
+final_size_data$demo_grp <- factor(
+  final_size_data$demo_grp,
+  levels = contact_data$demography$age.group
+)
+
+# multiply counts with proportion infected
+final_size_data$n_infected <- final_size_data$p_infected *
+  final_size_data$population
+# nolint end
