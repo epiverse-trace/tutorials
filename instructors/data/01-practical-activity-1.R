322b5a06a9ceeb61259a856c62b1a070da34156b
Andree Valle Campos <avallecam@gmail.com>
2026-06-20 14:05:33 +0100

    fix learners scaffold early 1 and 2

--- instructors/data/01-practical-activity-1.R
+++ instructors/data/01-practical-activity-1.R
@@ -7,30 +7,30 @@ room_number <- #<COMPLETE> replace with 1/2/3/4
 
 # Load packages ----------------------------------------------------------
 library(cleanepi)
-library(linelist)
-library(incidence2)
 library(tidyverse)
 
 
 # Adapt the data dictionary ----------------------------------------------
 
-# Replace 'variable_name' when you have the information
+# Replace '#<COMPLETE>' when you have the information
 dat_dictionary <- tibble::tribble(
-  ~options,  ~values,            ~grp, ~orders,
-       "1",   "male", "variable_name",      1L,
-       "2", "female", "variable_name",      2L,
-       "M",   "male", "variable_name",      3L,
-       "F", "female", "variable_name",      4L,
-       "m",   "male", "variable_name",      5L,
-       "f", "female", "variable_name",      6L
+  ~options,  ~values,          ~grp, ~orders,
+       "1",   "male", "#<COMPLETE>",      1L,
+       "2", "female", "#<COMPLETE>",      2L,
+       "M",   "male", "#<COMPLETE>",      3L,
+       "F", "female", "#<COMPLETE>",      4L,
+       "m",   "male", "#<COMPLETE>",      5L,
+       "f", "female", "#<COMPLETE>",      6L
 )
 
 dat_dictionary
 
 
 # Read raw data ----------------------------------------------------------
+
+# Replace the string with the URL provided (or location to local file)
 dat_raw <- readr::read_csv(
-  #<COMPLETE>
+  "paste/complete/URL/#<COMPLETE>" #<COMPLETE>
   )
 
 dat_raw
@@ -39,35 +39,51 @@ dat_raw
 # Clean and standardize data ---------------------------------------------
 
 # How many cleanepi functions did you use to get clean data?
+# Remove those functions not needed in the cleaning workflow
+
 dat_clean <- dat_raw %>%
-  cleanepi::#<COMPLETE>
-
-dat_clean
-
-
-# Create time span variable ----------------------------------------------
-
-# What time span unit best describes the 'delay' from 'onset' to 'death'?
-dat_timespan <- dat_clean %>%
-  cleanepi::timespan(
-    #<COMPLETE>
-    #<COMPLETE>
-    #<COMPLETE>
-    span_column_name = "timespan_variable",
-    span_remainder_unit = NULL
-  ) %>%
-  # skimr::skim(timespan_variable)
-  # Categorize the delay numerical variable
-  dplyr::mutate(
-    timespan_category = base::cut(
-      x = timespan_variable,
-      breaks = #<COMPLETE>, 
-      include.lowest = TRUE,
-      right = FALSE
+  cleanepi::standardize_column_names() %>%
+  cleanepi::standardize_dates(
+    target_columns = c(
+      #<COMPLETE>,
+      #<COMPLETE>,
+      #<COMPLETE>
     )
+  ) %>%
+  cleanepi::check_date_sequence(
+    target_columns = c(
+      #<COMPLETE>,
+      #<COMPLETE>,
+      #<COMPLETE>
+    )
+  ) %>%
+  cleanepi::convert_to_numeric(
+    target_columns = #<COMPLETE>
+  ) %>% 
+  # using data_dictionary requires valid missing entries
+  cleanepi::replace_missing_values(
+    target_columns = #<COMPLETE>,
+    na_strings = #<COMPLETE>
+  ) %>%
+  cleanepi::clean_using_dictionary(
+    dictionary = #<COMPLETE>
+  ) %>%
+  cleanepi::remove_constants() %>%
+  cleanepi::remove_duplicates(
+    target_columns = c(
+      #<COMPLETE>,
+      #<COMPLETE>
+    )
+  ) %>%
+  cleanepi::timespan(
+    target_column = #<COMPLETE>,
+    end_date = #<COMPLETE>,
+    span_unit = #<COMPLETE>,
+    span_column_name = #<COMPLETE>,
+    span_remainder_unit = #<COMPLETE>
   )
 
-dat_timespan
+dat_clean
 
 
 # nolint end
